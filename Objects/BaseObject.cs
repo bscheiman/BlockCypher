7b58209d0bf60594cb4ca646477ba03e39c62964
Boris Scheiman <bscheiman@gmail.com>
2015-04-22 11:49:35 -0500

    Errors update

--- Objects/BaseObject.cs
+++ Objects/BaseObject.cs
@@ -1,5 +1,8 @@
 ﻿#region
+using System.Collections.Generic;
+using System.Linq;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 #endregion
 
@@ -8,16 +11,20 @@ namespace BlockCypher.Objects {
         [JsonProperty("error")]
         public string Error { get; set; }
 
-        [JsonProperty("errors")]
-        public ErrorObj[] Errors { get; set; }
+        public string[] Errors {
+            get {
+                if (Extra == null || !Extra.ContainsKey("errors"))
+                    return null;
+
+                return Extra["errors"].Select(a => a["error"].ToString()).ToArray();
+            }
+        }
+
+        [JsonExtensionData]
+        private IDictionary<string, JToken> Extra { get; set; }
 
         public bool IsError {
             get { return !string.IsNullOrEmpty(Error) || Errors != null; }
         }
-    }
-
-    public class ErrorObj {
-        [JsonProperty("error")]
-        public string Error { get; set; }
     }
 }
